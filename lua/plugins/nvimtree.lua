19f65eb163e8c1d048b29b5659f59ac670e5e7b7
Svilen Vasilev <svsv@hey.com>
2023-02-05 23:57:45 +0200

    nvimtree width

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -45,7 +45,7 @@ require'nvim-tree'.setup {
   },
   view = {
     -- width of the window, can be either a number (columns) or a string in `%`, for left or right side placement
-    width = '25%',
+    width = '20%',
     -- height of the window, can be either a number (columns) or a string in `%`, for top or bottom side placement
     -- height = 30,
     -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
